b8cc9f90d1867027e934e67c74653415dc2dba8b
RumbleKAT <reki318@naver.com>
2022-04-28 23:30:41 +0900

    pug nodejs rendering

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
-type ResultType = [boolean, string];
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
 
-const doSomething = (): ResultType =>{
-    try{
-        throw new Error(`Some error occurs....`);
-    }catch(e:any){
-        return [false, e.message]
-    };
-};
+const app = express();
+app.set('view engine', 'pug');
+app.set('views',path.join(__dirname,'template'));
+app.use(express.static(path.join(__dirname,'template')));
 
-const [result, errorMessage] = doSomething();
-console.log(result, errorMessage);
+const port = 8081;
+
+app.get('/',(req:Request, res:Response,next:NextFunction)=>{
+    res.render('index');
+});
+
+app.listen(port,()=>{
+    console.log(`server is running on port : ${port}`);
+});
